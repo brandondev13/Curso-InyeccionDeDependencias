ed0ff371dc79f9f6ca6c6f5908ed0aa8b054350f
Brandon Mejia <brandonmejiarestrepo@gmail.com>
2022-12-05 15:34:54 -0500

    Anotaciones condicionales

--- InyeccionDeDependencias_Ioc/src/main/java/com/inyeccion/controller/HelloController.java
+++ InyeccionDeDependencias_Ioc/src/main/java/com/inyeccion/controller/HelloController.java
@@ -7,13 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${app.name}")
-    private String appName;
-
-
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World " + appName;
+        return "Hello World " ;
     }
 
 }
